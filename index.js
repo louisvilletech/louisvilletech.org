fdb523d1cfc4bc99a2fa603d45ec26be0f2afc87
Eric Lathrop <eric@ericlathrop.com>
2015-07-02 20:06:30 -0400

    Switch from ical2json to ical module
    Implements recurring events, usually seen on Google Calendar.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var path = require("path");
-var ical2json = require("ical2json");
+var ical = require("ical");
 var groups = require("./data/groups");
 var R = require("ramda");
 
@@ -9,9 +9,9 @@ var eventJson = "data/events.json";
 
 var events = R.pipe(
 	R.reject(hiddenFile),
-	R.map(loadCal),
-	R.flatten,
-	R.sortBy(startTime)
+	R.chain(loadCal),
+	R.sortBy(R.prop("start")),
+	R.reject(R.compose(isOld, R.prop("end")))
 )(fs.readdirSync(icsDir));
 
 writeJson(eventJson, events);
@@ -21,24 +21,32 @@ function hiddenFile(file) {
 }
 
 function loadCal(filename) {
-	var cal = cal2vcal(ical2json.convert(readUtf8(wholePath(filename))));
 	var group = filename.substr(0, filename.indexOf("."));
+	return R.pipe(
+		R.values,
+		R.filter(R.propEq("type", "VEVENT")),
+		R.chain(expandRecurrences),
+		R.map(enhanceEvent.bind(this, group))
+	)(ical.parseFile(wholePath(filename)));
+}
 
-	var events = [];
-	if (cal) {
-		events = R.pipe(
-			R.flatten,
-			R.map(cal2tz),
-			R.flatten,
-			R.map(tz2event),
-			R.flatten,
-			R.map(fixEvent.bind(undefined, group)),
-			R.sortBy(startTime),
-			R.reject(isOld)
-		)(cal);
+function expandRecurrences(event) {
+	var future = new Date(Date.now());
+	future.setDate(future.getDate() + 365); // adding years gives you a date in the year "116", so just add days. leap years aren't important, we just want a rough future date
+
+	if (event.rrule === undefined) {
+		return [event];
 	}
-	writeJson("data/group-events/" + group + ".json", events);
-	return events;
+	return event.rrule.all(function(date, i) {
+		return date < future;
+	}).map(function(date) {
+		var copy = R.clone(event);
+		var duration = copy.end - copy.start;
+		copy.start = date;
+		copy.end = new Date(date);
+		copy.end.setMilliseconds(copy.end.getMilliseconds() + duration);
+		return copy;
+	});
 }
 
 function writeJson(path, data) {
@@ -48,98 +56,23 @@ function writeJson(path, data) {
 function wholePath(file) {
 	return path.join(icsDir, file);
 }
-function readUtf8(path) {
-	return fs.readFileSync(path, { encoding: 'UTF8' });
-}
-function cal2vcal(cal) {
-	return cal.VCALENDAR;
-}
-function cal2tz(cal) {
-	return cal.VTIMEZONE || [];
-}
-function tz2event(tz) {
-	if (tz.VTIMEZONE) {
-		tz = tz.VTIMEZONE[0];
-	}
-	return tz.VEVENT;
-}
 
-function isOld(event) {
-	var start = startTime(event);
-
+function isOld(date) {
 	var now = new Date(Date.now());
 	now.setDate(now.getDate() - 1);
-
-	return start < now;
+	return date < now;
 }
 
-function startTime(event) {
-	for (var prop in event) {
-		if (event.hasOwnProperty(prop)) {
-			if (prop.indexOf("DTSTART") === 0) {
-				return toDate(event[prop]);
-			}
-		}
+function enhanceEvent(group, event) {
+	if (event.location) {
+		event.mapQuery = event.location.replace("(", " ").replace(")", " ");
 	}
-}
 
-function endTime(event) {
-	for (var prop in event) {
-		if (event.hasOwnProperty(prop)) {
-			if (prop.indexOf("DTEND") === 0) {
-				return toDate(event[prop]);
-			}
-		}
-	}
-}
-
-function toDate(timeStr) {
-	var parts = timeStr.split(":");
-
-	var tz = "US-Eastern";
-	var datetime = timeStr;
-	if (parts.length > 1) {
-		if (parts.indexOf("TZID=") === 0) {
-			tz = parts.substr(5);
-		}
-		datetime = parts[1];
-	}
-	parts = datetime.split("T");
-	var date = parts[0];
-	var time = parts[1] || "";
-
-	var year = parseInt(date.substr(0, 4));
-	var month = parseInt(date.substr(4, 2)) - 1;
-	var day = parseInt(date.substr(6, 2));
-	var hour = parseInt(time.substr(0, 2)) || 0;
-	var min = parseInt(time.substr(2, 2)) || 0;
-	var sec = parseInt(time.substr(4, 2)) || 0;
-	if (time[6] === "Z") {
-		return new Date(Date.UTC(year, month, day, hour, min, sec));
-	} else {
-		return new Date(year, month, day, hour, min, sec);
-	}
-}
-
-function fixText(text) {
-	if (text) {
-		return text.replace(/\\n/g, "\n").replace(/\\,/g, ",");
-	}
-	return text;
-}
-
-function fixEvent(group, event) {
-	event.SUMMARY = fixText(event.SUMMARY);
-	event.DESCRIPTION = fixText(event.DESCRIPTION);
-	event.LOCATION = fixText(event.LOCATION);
-	if (event.LOCATION) {
-		event.mapQuery = event.LOCATION.replace("(", " ").replace(")", " ");
-	}
-	var start = startTime(event);
+	var start = event.start;
 	event.startDate = start.toString();
 	event.startDateJson = start.toJSON();
 
-	var end = endTime(event);
+	var end = event.end;
 	event.endDate = end.toString();
 	event.endDateJson = end.toJSON();
 	if (end < Date.now()) {
@@ -149,5 +82,8 @@ function fixEvent(group, event) {
 	event.group = group;
 	event.groupName = groups[group].name;
 	event.groupUrl = groups[group].web;
+
+	delete event.rrule;
+
 	return event;
 }
