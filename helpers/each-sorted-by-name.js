d0d03f9816425c22ad5316b5337e4cc55053c64e
Eric Lathrop <eric@ericlathrop.com>
2018-11-07 21:02:07 -0500

    Tabs to spaces

--- helpers/each-sorted-by-name.js
+++ helpers/each-sorted-by-name.js
@@ -1,9 +1,9 @@
 "use strict";
 
 module.exports = function(context, options) {
-	return Object.keys(context).sort(function(a, b) {
-		return context[a].name.localeCompare(context[b].name);
-	}).reduce(function(ret, key) {
-		return ret + options.fn(context[key], { data: { key: key } });
-	}, "");
+  return Object.keys(context).sort(function(a, b) {
+    return context[a].name.localeCompare(context[b].name);
+  }).reduce(function(ret, key) {
+    return ret + options.fn(context[key], { data: { key: key } });
+  }, "");
 };
